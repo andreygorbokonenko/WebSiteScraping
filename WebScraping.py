4a40294e9359d153ea5cfe080f14927e26fa0c44
andreygorbokonenko <andreygorbokonenko@gmail.com>
2024-12-29 10:33:15 -0700

    Add files via upload

---
+++ WebScraping.py
@@ -0,0 +1,9 @@
+
+# Importing necessary libraries
+
+import pandas as pd
+from lxml import etree as et
+from bs4 import BeautifulSoup
+from selenium import webdriver
+pd.options.mode.chained_assignment = None
+from selenium.webdriver.common.by import By
